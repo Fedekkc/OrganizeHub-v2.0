9a5aea4779117d7999be74d23a773274b2034395
Federico Cacace <cacacefederico05@gmail.com>
2024-09-22 17:23:29 -0300

    Error handling

--- back/src/controllers/user.controller.ts
+++ back/src/controllers/user.controller.ts
@@ -1,42 +1,85 @@
-import { Controller, Get, Post, Put, Delete, Body, SetMetadata } from "@nestjs/common";
+import { Controller, Get, Post, Put, Delete, Body, Param, HttpException, HttpStatus, SetMetadata } from "@nestjs/common";
 import { UserDTO } from "../dtos/user.dto";
 import { UserService } from "../services/user.service";
 import { User } from "../entities/user.entity";
-import { PartialType } from "@nestjs/swagger";
-
+import { EntityNotFoundError } from "typeorm";
 
 @Controller('users')
 export class UserController {
     constructor(private userService: UserService) {}
-    
+
     @SetMetadata('isPublic', false)
     @Get()
     async getAllUsers(): Promise<User[]> {
-        return await this.userService.getAllUsers();
+        try {
+            return await this.userService.getAllUsers();
+        } catch (error) {
+            if (error instanceof EntityNotFoundError) {
+                throw new HttpException('Users not found', HttpStatus.NOT_FOUND);
+            }
+            throw new HttpException('Failed to get users', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @SetMetadata('isPublic', false)    
+    @SetMetadata('isPublic', false)
     @Get(':userId')
-    async getUserById(userId: number): Promise<User> {
-        return await this.userService.getUserById(userId);
+    async getUserById(@Param('userId') userId: number): Promise<User> {
+        try {
+            const user = await this.userService.getUserById(userId);
+            if (!user) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            return user;
+        } catch (error) {
+            if (error instanceof EntityNotFoundError) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            throw new HttpException('Failed to get user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @SetMetadata('isPublic', false)
     @Post()
     async createUser(@Body() userDTO: UserDTO): Promise<User> {
-        return await this.userService.createUser(userDTO);
+        try {
+            return await this.userService.createUser(userDTO);
+        } catch (error) {
+            if (error instanceof EntityNotFoundError) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            throw new HttpException('Failed to create user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @SetMetadata('isPublic', false)
     @Put(':userId')
-    async updateUser(userId: number, @Body() userDTO: UserDTO): Promise<User> {
-        return await this.userService.updateUser(userId, userDTO);
+    async updateUser(@Param('userId') userId: number, @Body() userDTO: UserDTO): Promise<User> {
+        try {
+            const user = await this.userService.updateUser(userId, userDTO);
+            if (!user) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            return user;
+        } catch (error) {
+            if (error instanceof EntityNotFoundError) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            throw new HttpException('Failed to update user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Delete(':userId')
-    async deleteUser(userId: number): Promise<void> {
-        return await this.userService.deleteUser(userId);
+    async deleteUser(@Param('userId') userId: number): Promise<void> {
+        try {
+            const result = await this.userService.deleteUser(userId);
+            if (!result) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+        } catch (error) {
+            if (error instanceof EntityNotFoundError) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            throw new HttpException('Failed to delete user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-
-
 }
--- back/src/services/user.service.ts
+++ back/src/services/user.service.ts
@@ -2,7 +2,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "../entities/user.entity";
 import { UserDTO } from "../dtos/user.dto";
-import { Injectable } from "@nestjs/common";
+import { Injectable, HttpException, HttpStatus } from "@nestjs/common";
 import { hash } from "bcrypt";
 
 @Injectable()
@@ -12,34 +12,66 @@ export class UserService {
     ) {}
 
     async getAllUsers(): Promise<User[]> {
-        return await this.userRepository.find();
+        try {
+            return await this.userRepository.find();
+        } catch (error) {
+            throw new HttpException('Failed to get users', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     async getUserById(userId: number): Promise<User> {
-        return await this.userRepository.findOne( { where: {userId} } );
+        try {
+            const user = await this.userRepository.findOne({ where: { userId } });
+            if (!user) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            return user;
+        } catch (error) {
+            throw new HttpException('Failed to get user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     async createUser(userDTO: UserDTO): Promise<User> {
-        const user = new User();
-        user.firstName = userDTO.firstName;
-        user.lastName = userDTO.lastName;
-        user.username = userDTO.username;
-        user.email = userDTO.email;
-        user.password = await hash(userDTO.password, 10);
-        return await this.userRepository.save(user);
+        try {
+            const user = new User();
+            user.firstName = userDTO.firstName;
+            user.lastName = userDTO.lastName;
+            user.username = userDTO.username;
+            user.email = userDTO.email;
+            user.password = await hash(userDTO.password, 10);
+            return await this.userRepository.save(user);
+        } catch (error) {
+            throw new HttpException('Failed to create user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     async updateUser(userId: number, userDTO: UserDTO): Promise<User> {
-        const user = await this.userRepository.findOne( { where: {userId} } );
-        user.firstName = userDTO.firstName;
-        user.lastName = userDTO.lastName;
-        user.username = userDTO.username;
-        user.email = userDTO.email;
-        user.password = await hash(userDTO.password, 10);
-        return await this.userRepository.save(user);
+        try {
+            const user = await this.userRepository.findOne({ where: { userId } });
+            if (!user) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            user.firstName = userDTO.firstName;
+            user.lastName = userDTO.lastName;
+            user.username = userDTO.username;
+            user.email = userDTO.email;
+            user.password = await hash(userDTO.password, 10);
+            return await this.userRepository.save(user);
+        } catch (error) {
+            throw new HttpException('Failed to update user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    async deleteUser(userId: number): Promise<void> {
-        await this.userRepository.delete(userId);
+    async deleteUser(userId: number): Promise<boolean> {
+        try {
+            const result = await this.userRepository.delete(userId);
+            if (result.affected === 0) {
+                throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+            }
+            return true;
+        } catch (error) {
+            throw new HttpException('Failed to delete user', HttpStatus.INTERNAL_SERVER_ERROR);
+            return false;
+        }
     }
 }
